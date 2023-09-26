7d3036adf5a52a52ca70819fa569052a5c45f394
Test <test@test.pl>
2023-09-26 16:28:25 +0200

    zadanie

---
+++ programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
@@ -0,0 +1,29 @@
+﻿//1. Program obliczający średnią artmetyczną 2 liczb
+//2. Program obliczający pole prostokąta.
+//3. Program obliczający objętość stożka.
+//4. Program obliczający pole koła.
+//5. Program obliczający wartość wyrażenia a ^ 2 + b ^ 2
+//6. Program obliczający pole trójkąta o podstawie b i wysokości h
+//7. Program obliczający objętość kuli o promieniu r
+//8. Program obliczający pole trapezu o podstawach a i b oraz wysokości h
+//9. Program obliczający średnią ważoną trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+
+#include <iostream>
+
+using namespace std;
+
+void task1()
+{
+	double FirstNumber, SecondNumber;
+	cout << "insert First number:\n";
+	cin >> FirstNumber;
+	cout << "insert Second number:\n";
+	cin >> SecondNumber;
+}
+
+int main()
+{
+	task1();
+
+	return 0;
+}
