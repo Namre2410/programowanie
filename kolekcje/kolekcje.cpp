8adcfcb120a25b6c67a82e25c7e2ca7c5d0a20b9
Namre2410 <144441006+Namre2410@users.noreply.github.com>
2023-11-24 13:08:51 +0100

    Update kolekcje.cpp

--- kolekcje/kolekcje.cpp
+++ kolekcje/kolekcje.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 /*
-* Napisz program, który będzie prosił o hasło. Nie przepuści dalej dopóki nie zostanie ono podane prawidłowo.
 * Napisz program, który pobiera od użytkownika ciąg znaków i wyświetla liczbę samogłosek i spółgłosek w tym ciągu.
 * Poproś użytkownika o wprowadzenie liczby całkowitej w systemie dziesiętnym. Następnie skonwertuj tę liczbę na system dwójkowy (binarny) i wyświetl wynik.
 * Program sprawdzający czy podany ciąg znaków jest palindromem (czyli takim, który czytany od tyłu jest taki sam, jak czytany od przodu, np. "kajak")
@@ -50,7 +49,71 @@ void task2()
 	cout << "witaj " << userName << " tutaj. \n";
 }
 
+void task3()
+{
+    char signFromUser;
+    //signFromUser = 'a';
+    cout << "Podaj znak\n";
+    cin >> signFromUser;
+
+    cout << "Podałeś znak " << signFromUser << "\n";
+
+    //if (signFromUser >= 97 && signFromUser <= 122)
+    if (signFromUser >= 'a' && signFromUser <= 'z')
+        cout << "Znak jest małą literą alfabetu\n";
+    else
+        cout << "Znak nie jest małą literą alfabetu\n";
+
+    signFromUser = signFromUser + 2;
+    cout << "Podałeś znak " << signFromUser << "\n";
+
+    signFromUser = 'a' + 2;
+    cout << "Podałeś znak " << signFromUser << "\n";
+}
+
+
+//Napisz program, który poprosi cie o twoje imię i cię przywita.
+void task4()
+{
+    string userName;
+    //userName = "Ala";
+    cout << "Podaj swoje imię:\n";
+    cin >> userName;
+
+    cout << "Witaj " << userName << " tutaj.\n";
+}
+
+//Napisz program, który policzy ile w napisie jest liter 'a'.
+void task5()
+{
+    string textFromUser;
+    cout << "Podaj tekst\n";
+    cin >> textFromUser;
+
+    //textFromUser = "";
+    //textFromUser[0] = 'h';
+    //std::cout << textFromUser[1] << "\n";
+    //std::cout << textFromUser.length() << "\n"; 
+    //if (textFromUser == "Ala")
+
+    int counter = 0;
+    for (int i = 0; i < textFromUser.length(); i++)
+    {
+        if (textFromUser[i] == 'a')
+            counter++;
+    }
+
+    std::cout << "Małych liter 'a' jest " << counter << "\n";
+}
+
+// Napisz program, który będzie prosił o hasło. Nie przepuści dalej dopóki nie zostanie ono podane prawidłowo.
+void task()
+{
+
+
+}
+
 int main()
 {
-	task2();
+	task5();
 }
