062781ff397cc9a9b776bd27a76626913eb999c0
Namre2410 <144441006+Namre2410@users.noreply.github.com>
2023-11-24 13:03:08 +0100

    Update kolekcje.cpp

--- kolekcje/kolekcje.cpp
+++ kolekcje/kolekcje.cpp
@@ -1,20 +1,56 @@
-﻿// kolekcje.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+/*
+* Napisz program, który będzie prosił o hasło. Nie przepuści dalej dopóki nie zostanie ono podane prawidłowo.
+* Napisz program, który pobiera od użytkownika ciąg znaków i wyświetla liczbę samogłosek i spółgłosek w tym ciągu.
+* Poproś użytkownika o wprowadzenie liczby całkowitej w systemie dziesiętnym. Następnie skonwertuj tę liczbę na system dwójkowy (binarny) i wyświetl wynik.
+* Program sprawdzający czy podany ciąg znaków jest palindromem (czyli takim, który czytany od tyłu jest taki sam, jak czytany od przodu, np. "kajak")
+* Program sprawdzający czy podane dwa słowa są anagramami (czyli takimi, które zawierają te same litery, ale w innym układzie, np. "klasa" i "salka")
+*
+
+* Program wyciągający informacje z numeru PESEL
+* Program implementujący algorytm szyfrowania Cezara (proste szyfrowanie, w którym każdy znak w tekście jest zastępowany innym znakiem, przesuniętym o stałą liczbę pozycji w alfabecie).
+
+
+* Program który na wejściu przyjmie równanie a na wyjściu da równanie w odwrotnej notacji polskiej ONP. Np. na wejściu 2+3*4 na wyjścu da 234*+
+* Program, który na wejściu przyjmie rówanie w ONP a na wyjściu wyświetli wynik rówania.
+*/
+
+//Napisz program, który wczyta znak z klawiatury i sprawdzi czy jest to mała litera aldfabetu.
+
+void task1()
+{
+	char SignFromUser;
+	cout << "insert sign \n";
+	cin >> SignFromUser;
+
+	cout << "you inserted a sign" << SignFromUser << "\n";
+
+	if (SignFromUser >= 'a' && SignFromUser <= 'z')
+		cout << "the sign is a small letter of the alphabet\n";
+	else
+	{
+		cout << "the sign is not a small letter of the alphabet\n";
+	}
+
+	SignFromUser - SignFromUser + 2;
+	cout << "you inserted a sign" << SignFromUser << "\n";
+
+	SignFromUser - 'a' + 2;
+	cout << "you inserted a sign" << SignFromUser << "\n";
+}
+
+void task2()
+{
+	string userName;
+	cout << "insert your name:\n";
+	cin >> userName;
+
+	cout << "witaj " << userName << " tutaj. \n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	task2();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
